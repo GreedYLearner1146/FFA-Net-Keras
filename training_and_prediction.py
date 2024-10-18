47d0127d4ac781073c166dde6fb462fb13ce0970
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2024-10-19 02:57:24 +0800

    Update training_and_prediction.py

--- training_and_prediction.py
+++ training_and_prediction.py
@@ -1,4 +1,6 @@
 
+# Fill in the relevant image arrays and hyperparameters here.
+
 history = FFANet.fit('hazy train images array', 'clear train images array', batch_size='fill in here', epochs='fill in here', 
                      validation_data = ['hazy valid images array', 'clear valid images array'],shuffle=True)
 
