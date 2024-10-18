5794b4462cf15c5602c9ba57fd1c7de802818174
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2024-10-19 02:45:37 +0800

    Create training_and_prediction.py

---
+++ training_and_prediction.py
@@ -0,0 +1,5 @@
+
+history = FFANet.fit('hazy train images array', 'clear train images array', batch_size='fill in here', epochs='fill in here', 
+                     validation_data = ['hazy valid images array', 'clear valid images array'],shuffle=True)
+
+data_test_dehazed = FFANet.predict('hazy test images array')
